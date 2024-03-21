f2e7a81db0080c0a1a9005431696947366a732b7
MikeeClau3 <146947378+MikeeClau3@users.noreply.github.com>
2024-03-21 15:11:32 +0700

    initial commit

--- app/src/main/java/com/example/advweek4160421137/view/StudentListFragment.kt
+++ app/src/main/java/com/example/advweek4160421137/view/StudentListFragment.kt
@@ -5,56 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.advweek4160421137.R
+import com.example.advweek4160421137.databinding.FragmentStudentListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter  = StudentListAdapter(arrayListOf())
+    private lateinit var binding: FragmentStudentListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_list, container, false)
+        binding = FragmentStudentListBinding.inflate(inflater,container,false)
+        return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        //set layoutManager supaya bisa jalankan recycle view
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+    fun observeViewModel() {
+        //bertujuan untuk mendengarkan dari live data. jika data baru muncul, maka UI akan menanggapi
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            // it = berisi array student terbaru
+            studentListAdapter.updateStudentList(it)
+        })
+
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true){
+                binding.txtError.visibility = View.VISIBLE
+            }else{
+                binding.txtError.visibility = View.GONE
             }
+        })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
     }
 }

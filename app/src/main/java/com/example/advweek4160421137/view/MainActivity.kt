397080e5c914ad0bc2a224f2103cea8ae67ea24e
MikeeClau3 <146947378+MikeeClau3@users.noreply.github.com>
2024-03-21 14:24:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/advweek4160421137/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.advweek4160421137.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.advweek4160421137.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
